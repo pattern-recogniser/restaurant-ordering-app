de4931c4a75fe0ea47710e29ab9323b2c21ccc21
Anjali Karimpil <anjaliskarimpil@gmail.com>
2024-02-01 21:37:33 +0000

    Complted screen 1 - rendered from javascript, also added Thank you message in HTML

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # restaurant-ordering-app
+
+Requirements from [Figma file](https://scrimba.com/learn/frontend/solo-project-restaurant-ordering-app-co72e499baf5f48346e5975cf).
---
+++ index.js
@@ -0,0 +1,27 @@
+import { menuArray } from '/data.js'
+
+const menuEl = document.getElementsByTagName("menu")[0]
+
+function render(){
+    let htmlString = ""
+    for (let menuItem of menuArray){
+        const {name, ingredients, id, price, emoji} = menuItem
+        htmlString += `
+        <menu_item>
+            <p class="emoji-icon">${emoji}</p>
+            <div class="menu-info">
+                <h2>${name}</h2>
+                <p class="inner-p">${ingredients}</p>
+                <h3>\$${price}</h3>
+            </div>
+            <button>
+                <img src="images/add-btn.png" alt="add-btn">
+            </button>
+
+            </menu_item>
+        `
+    }
+    menuEl.innerHTML = htmlString
+}
+// render()
+
